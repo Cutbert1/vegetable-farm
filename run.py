9239c1572cc30904ae9b55591139df04e8882f84
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2024-10-30 13:58:41 +0000

    Updated code spacing errors with PEP8 python validator

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,19 +17,18 @@ SHEET = GSPREAD_CLIENT.open("vegetable_farm")
 def get_trade_data():
     """
     Get trade figures input from app users.
-    while loop repeatedly run for valid strings, 
+    while loop repeatedly run for valid strings,
     which must contain 13 numbers, separated with commas.
     """
     while True:
 
         print("Please type in trade data for last open market")
         print("Thirteen numbers separated by commas is expected")
-        print("For Example: 1343,1564,2675,3456,1985,6352,1853,5411,3452,1762,3286,1623,1527\n")
+        print("For Example: 134,156,27,345,198,63,183,51,342,172,386,12,127\n")
 
         data_str = input("Record data here: ")
-    
+
         trade_data = data_str.split(",")
-        
 
         if verify_data(trade_data):
             print("Data is valid...\n")
@@ -40,9 +36,10 @@ def get_trade_data():
 
     return trade_data
 
+
 def verify_data(values):
     """
-    All string values converted to integers inside the try. 
+    All string values converted to integers inside the try.
     If string values can't be converted to integer raise  ValueError.
     Raise ValueError  if integers are not 13 values exactly.
     """
@@ -56,26 +53,28 @@ def verify_data(values):
         print(f"Invalid data: {e}, try again please.\n")
         return False
 
-
     return True
 
-#def update_trade_worksheet(data):
+# def update_trade_worksheet(data):
     """
-    Access trade work sheet and add new row anytime new valid list data is entered
+    Access trade work sheet and add new row anytime new
+    valid data list is entered
     """
     print("Trade worksheet is being updated.....\n")
     trade_worksheet = SHEET.worksheet("trade")
     trade_worksheet.append_row(data)
     print("Trade worksheet successfully updated.\n")
 
-#def update_excess_worksheet(data):
+# def update_excess_worksheet(data):
     """
-    Access excess work sheet and add new row anytime new valid list data is entered and calculated
+    Access excess work sheet and add new row anytime new
+    valid data list is entered and calculated
     """
     print("Trade worksheet is being updated......\n")
     excess_worksheet = SHEET.worksheet("excess")
     excess_worksheet.append_row(data)
     print("Excess worksheet successfully updated.\n")
+
 
 def update_worksheet(data, worksheet):
     """
@@ -85,6 +84,7 @@ def update_worksheet(data, worksheet):
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} Worksheet successfully updated.\n")
+
 
 def calculate_excess_data(trade_row):
     """
@@ -98,13 +98,14 @@ def calculate_excess_data(trade_row):
     for harvest, trade in zip(harvest_row, trade_row):
         excess = int(harvest) - trade
         excess_data.append(excess)
-    
+
     return excess_data
+
 
 def get_last_week_trade():
     """
     Assembles columns of data in trade worksheet,
-    pulling last 7 entries (one week) for each vegetable 
+    pulling last 7 entries (one week) for each vegetable
     then return columns data.
     """
     trade = SHEET.worksheet("trade")
@@ -113,14 +114,16 @@ def get_last_week_trade():
     for ind in range(1, 14):
         column = trade.col_values(ind)
         columns.append(column[-7:])
-    
+
     return columns
+
 
 def calculate_harvest_data(data):
     """
-    Evaluate average harvest for each vegetable type, with addition of 20%
+    Evaluate average harvest for each vegetable type,
+    with addition of 20% forecast
     """
-    print("Evaluating harvest data....\n")
+    print("Evaluating harvest forecast data....\n")
     new_harvest_data = []
 
     for column in data:
@@ -144,6 +147,7 @@ def main():
     trade_columns = get_last_week_trade()
     harvest_data = calculate_harvest_data(trade_columns)
     update_worksheet(harvest_data, "harvest")
+
 
 print("Welcome to Vegetable Farm Data Automation")
 main()
